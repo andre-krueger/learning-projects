f2676d5bb942cf4236de8edb93686d4562d7c8cc
André Krüger <zeldamer@t-online.de>
2017-03-19 14:45:50 +0100

    update

--- java/tetromino/core/src/com/tetromino/game/Tetromino.java
+++ java/tetromino/core/src/com/tetromino/game/Tetromino.java
@@ -3,6 +3,7 @@ package com.tetromino.game;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by andre on 3/18/17.
@@ -10,27 +11,183 @@ import java.util.List;
 public class Tetromino {
     private int rotation;
     final LinkedHashMap<String, List<int[][]>> tetrominos;
+    int row;
+    int col;
+    String key;
+    private boolean canMoveLeft = true;
+    private boolean canMoveRight = true;
 
     public Tetromino() {
+        row = 0;
+        col = 4;
+
         ArrayList<int[][]> i = new ArrayList<>();
         i.add(new int[][]{
-                {1, 1, 1, 1}
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 1, 1},
+                {0, 0, 0, 0},
         });
         i.add(new int[][]{
-                {1},
-                {1},
-                {1},
-                {1},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
         });
+
+        ArrayList<int[][]> o = new ArrayList<>();
+        o.add(new int[][]{
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 1, 1, 0},
+                {0, 0, 0, 0},
+        });
+
+        ArrayList<int[][]> t = new ArrayList<>();
+        t.add(new int[][]{
+                {1, 1, 1},
+                {0, 1, 0},
+        });
+        t.add(new int[][]{
+                {0, 1},
+                {1, 1},
+                {0, 1}
+        });
+        t.add(new int[][]{
+                {0, 1, 0},
+                {1, 1, 1},
+        });
+        t.add(new int[][]{
+                {1, 0},
+                {1, 1},
+                {1, 0},
+        });
+
+        ArrayList<int[][]> j = new ArrayList<>();
+        j.add(new int[][]{
+                {1, 1, 1},
+                {0, 0, 1},
+        });
+        j.add(new int[][]{
+                {0, 1},
+                {0, 1},
+                {1, 1}
+        });
+        j.add(new int[][]{
+                {1, 0, 0},
+                {1, 1, 1},
+        });
+        j.add(new int[][]{
+                {1, 1},
+                {1, 0},
+                {1, 0},
+        });
+        ArrayList<int[][]> l = new ArrayList<>();
+        l.add(new int[][]{
+                {1, 1, 1},
+                {1, 0, 0},
+        });
+        l.add(new int[][]{
+                {1, 1},
+                {0, 1},
+                {0, 1}
+        });
+        l.add(new int[][]{
+                {0, 0, 1},
+                {1, 1, 1},
+        });
+        l.add(new int[][]{
+                {1, 0},
+                {1, 0},
+                {1, 1},
+        });
+
+        ArrayList<int[][]> s = new ArrayList<>();
+        s.add(new int[][]{
+                {0, 1, 1},
+                {1, 1, 0},
+        });
+        s.add(new int[][]{
+                {1, 0},
+                {1, 1},
+                {0, 1}
+        });
+
+        ArrayList<int[][]> z = new ArrayList<>();
+        z.add(new int[][]{
+                {1, 1, 0},
+                {0, 1, 1},
+        });
+        z.add(new int[][]{
+                {0, 1},
+                {1, 1},
+                {1, 0}
+        });
+
         tetrominos = new LinkedHashMap<>();
         tetrominos.put("i", i);
+        tetrominos.put("o", o);
+        tetrominos.put("t", t);
+        tetrominos.put("j", j);
+        tetrominos.put("l", l);
+        tetrominos.put("s", s);
+        tetrominos.put("z", z);
+
+        key = "";
+
+        Random r = new Random();
+
+        String alphabet = "i";
+
+        for (int h = 0; h < 1; h++) {
+            key = alphabet.charAt(r.nextInt(alphabet.length())) + "";
+        }
     }
 
     public int[][] getShape() {
-        return tetrominos.get("i").get(rotation);
+        if (rotation >= tetrominos.get(key).size()) {
+            rotation = 0;
+        }
+        return tetrominos.get(key).get(rotation);
     }
+
     public void rotateShape() {
-        
         rotation++;
     }
+
+    public void increaseRow() {
+        row++;
+    }
+
+    public void increaseCol() {
+        col++;
+    }
+
+    public void decreaseCol() {
+        col--;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCanMoveLeft(final boolean _canMoveLeft) {
+        canMoveLeft = _canMoveLeft;
+    }
+    public void setCanMoveRight(final boolean _canMoveRight) {
+        canMoveRight = _canMoveRight;
+    }
+
+    public boolean getCanMoveRight() {
+        return canMoveRight;
+    }
+
+    public boolean getCanMoveLeft() {
+        return canMoveLeft;
+    }
 }
+
--- java/tetromino/core/src/com/tetromino/game/screens/GameScreen.java
+++ java/tetromino/core/src/com/tetromino/game/screens/GameScreen.java
@@ -14,26 +14,25 @@ import com.tetromino.game.TetrominoGame;
 public class GameScreen implements Screen {
     final TetrominoGame game;
     final Texture tetrominoTexture;
-    int[][] grid;
-    int shapeCol;
-    int shapeRow;
+    final int[][] grid;
+
     private Tetromino tetromino;
 
     public GameScreen(final TetrominoGame game) {
         this.game = game;
         tetrominoTexture = new Texture(Gdx.files.internal("block.png"));
         grid = new int[][]{
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 1, 0, 0, 0, 1, 0, 0, 0, 0},
@@ -45,7 +44,8 @@ public class GameScreen implements Screen {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
         };
 
-        placeTetronimo();
+        tetromino = new Tetromino();
+
     }
 
     @Override
@@ -56,33 +56,35 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
-            if (!tetronimoCollided()) {
-                ++shapeRow;
-            } else {
-                placeTetronimo();
-            }
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-            if (!tetronimoCollided()) {
+
+        if (!tetrominoLanded()) {
+            if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+                tetromino.increaseRow();
+            } else if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
                 tetromino.rotateShape();
-            } else {
-                placeTetronimo();
+            } else if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+                tetrominoCollided();
+                if (!tetromino.getCanMoveRight()) {
+                    tetromino.setCanMoveRight(true);
+                }
+                if (tetromino.getCanMoveLeft()) {
+                    tetromino.increaseCol();
+                }
+
+            } else if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+                tetrominoCollided();
+                if (!tetromino.getCanMoveLeft()) {
+                    tetromino.setCanMoveLeft(true);
+                }
+                if (tetromino.getCanMoveRight()) {
+                    tetromino.decreaseCol();
+                }
             }
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
-            if (!tetronimoCollided()) {
-                ++shapeCol;
-            } else {
-                placeTetronimo();
-            }
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
-            if (!tetronimoCollided()) {
-                --shapeCol;
-            } else {
-                placeTetronimo();
-            }
+        } else {
+            tetromino = new Tetromino();
         }
         game.batch.begin();
-        renderTetronimo();
+        renderTetromino();
         renderGrid();
         game.batch.end();
     }
@@ -122,28 +124,25 @@ public class GameScreen implements Screen {
         }
     }
 
-    private void placeTetronimo() {
-        tetromino = new Tetromino();
-        shapeRow = 0;
-        shapeCol = 5;
-    }
-
-    private void renderTetronimo() {
+    private void renderTetromino() {
         for (int i = 0; i < tetromino.getShape().length; i++) {
             for (int j = 0; j < tetromino.getShape()[i].length; j++) {
                 if (tetromino.getShape()[i][j] != 0) {
-                    game.batch.draw(tetrominoTexture, (j * -15) - (shapeCol * 15) + 300, (i * -15) - (shapeRow * 15) + 300);
+                    game.batch.draw(
+                            tetrominoTexture,
+                            (j * -15) - (tetromino.getCol() * 15) + 300,
+                            (i * -15) - (tetromino.getRow() * 15) + 300
+                    );
                 }
             }
         }
     }
 
-    private boolean tetronimoCollided() {
+    private boolean tetrominoLanded() {
         for (int row = 0; row < tetromino.getShape().length; row++) {
             for (int col = 0; col < tetromino.getShape()[row].length; col++) {
                 if (tetromino.getShape()[row][col] != 0) {
-                    if (grid[row + shapeRow + 1][col + shapeCol] != 0) {
-                        System.out.println("ee");
+                    if (grid[row + tetromino.getRow() + 1][col + tetromino.getCol()] != 0) {
                         addTetrominoToGrid();
                         return true;
                     }
@@ -157,13 +156,34 @@ public class GameScreen implements Screen {
         for (int row = 0; row < tetromino.getShape().length; row++) {
             for (int col = 0; col < tetromino.getShape()[row].length; col++) {
                 if (tetromino.getShape()[row][col] != 0) {
-                    grid[row + shapeRow][col + shapeCol] = tetromino.getShape()[row][col];
+                    grid[row + tetromino.getRow()][col + tetromino.getCol()] = tetromino.getShape()[row][col];
                 }
             }
         }
     }
 
-    private void rotateTetromino() {
+    private void tetrominoCollided() {
+        for (int row = 0; row < tetromino.getShape().length; row++) {
+            for (int col = 0; col < tetromino.getShape()[row].length; col++) {
+                if (tetromino.getShape()[row][col] != 0) {
+                    if (col + tetromino.getCol() - 1 <= 0) {
+                        tetromino.setCanMoveRight(false);
+                    }
+                    System.out.println(tetromino.getCol() + col);
+                    if (col + tetromino.getShape().length + tetromino.getCol()  >= grid[0].length) {
+                        tetromino.setCanMoveLeft(false);
+                    }
+                    if (grid[row + tetromino.getRow()][col + tetromino.getCol()-1] != 0) {
+                        tetromino.setCanMoveRight(false);
+                    }
+                    if (grid[row + tetromino.getRow()][col + tetromino.getCol()+1] != 0) {
+                        tetromino.setCanMoveLeft(false);
+                    }
 
+
+                }
+            }
+        }
     }
 }
+
